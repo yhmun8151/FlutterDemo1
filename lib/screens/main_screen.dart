2a58288e52751cc3ff6bb887c9a47806a9f132ad
jinyoung <yhmun8151@gmail.com>
2022-01-31 22:14:56 +0900

    1/31

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginSignupScreen extends StatefulWidget {
+  const LoginSignupScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginSignupScreenState createState() => _LoginSignupScreenState();
+}
+
+class _LoginSignupScreenState extends State<LoginSignupScreen> {
+  @override
+  Widget build (BuildContext context) {
+    return Container();
+  }
+}
